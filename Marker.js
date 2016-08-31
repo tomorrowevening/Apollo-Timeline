485e6a2b40ffadb2fc36cd29c0798cbadb337e04
Colin Duffy <colin@tomorrowevening.com>
2016-08-30 17:05:36 -0700

    Markers can now call functions

--- Marker.js
+++ Marker.js
@@ -3,10 +3,11 @@
  * @author Colin Duffy
  */
 
-function Marker(name, time, action) {
-    this.name    = name   !== undefined ? name   : "";
-    this.time    = time   !== undefined ? time   : 0;
-    this.action  = action !== undefined ? action : "";
+function Marker(name, time, action, trigger) {
+    this.name    = name    !== undefined ? name    : "";
+    this.time    = time    !== undefined ? time    : 0;
+    this.action  = action  !== undefined ? action  : "";
+    this.trigger = trigger !== undefined ? trigger : function() {};
     return this;
 };
 
